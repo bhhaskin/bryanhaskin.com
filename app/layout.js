fe37bc0e57805348a0160d66a95288cc1eb3fee0
Bryan Haskin <bhhaskin@bitsofsimplicity.com>
2024-03-13 11:53:05 -0700

    added og tags and card

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,23 @@ import "./globals.scss";
 
 export const metadata = {
   title: "Bryan Haskin | Coder | Sailor | Maker",
-  description: " I write code and solve problems, sometimes even in that order.",
+  description: "I write code and solve problems, sometimes even in that order.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta property="og:site_name" content="Bryan Haskin | Coder | Sailor | Maker" />
+      <meta property="og:title" content="Bryan Haskin | Coder | Sailor | Maker" />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content="I write code and solve problems, sometimes even in that order." />
+      <meta property="og:image" content="/assets/images/card.jpg" />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:image:width" content="1280" />
+      <meta property="og:image:height" content="800" />
+      <meta property="og:url" content="https://bryanhaskin.com" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <link rel="canonical" href="https://bryanhaskin.com" />
       <meta name="color-scheme" content="light only" />
       <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png"/>
