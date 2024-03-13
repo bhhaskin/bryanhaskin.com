46e7d2950b60630a197e3664301805c1b6e7da1b
Bryan Haskin <bhhaskin@bitsofsimplicity.com>
2024-03-13 00:05:19 -0700

    mobile, robots and sitemap

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
 import styles from "./page.module.scss";
+import GithubIcon from "@/components/svgs/github";
+import MastodonIcon from "@/components/svgs/mastodon";
+import BlogIcon from "@/components/svgs/blog";
 
 export default function Home() {
   return (
@@ -9,6 +12,15 @@ export default function Home() {
           <strong>Coder</strong> | <strong>Sailor</strong> | <strong>Maker</strong><br/>
           I write code and solve problems
         </p>
+        <ul className={styles['page__icon-list']}>
+          <li><a href="https://github.com/bhhaskin" aria-label="GitHub">
+            <GithubIcon/>
+            <span>Github</span>
+            </a></li>
+          <li><a rel="me" href="https://social.bitsofsimplicity.com/@bhhaskin" aria-label="Mastodon"><MastodonIcon/>
+          <span>Mastodon</span></a></li>
+          <li><a href="https://bitsofsimplicity.com/"><BlogIcon/><span>Blog</span></a></li>
+        </ul>
       </div>
     </main>
   );
