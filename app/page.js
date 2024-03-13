1a0d2eb00cce4be2b7609a25b0ae6ff00e360d63
Bryan Haskin <bhhaskin@bitsofsimplicity.com>
2024-03-13 10:27:01 -0700

    added cv and linkedin

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import styles from "./page.module.scss";
 import GithubIcon from "@/components/svgs/github";
 import MastodonIcon from "@/components/svgs/mastodon";
 import BlogIcon from "@/components/svgs/blog";
+import CvIcon from "@/components/svgs/cv";
+import LinkedIn from "@/components/svgs/linkedin";
 
 export default function Home() {
   return (
@@ -13,13 +15,36 @@ export default function Home() {
           I write code and solve problems
         </p>
         <ul className={styles['page__icon-list']}>
-          <li><a href="https://github.com/bhhaskin" aria-label="GitHub">
+          <li>
+            <a href="https://github.com/bhhaskin" aria-label="GitHub">
             <GithubIcon/>
             <span>Github</span>
-            </a></li>
-          <li><a rel="me" href="https://social.bitsofsimplicity.com/@bhhaskin" aria-label="Mastodon"><MastodonIcon/>
-          <span>Mastodon</span></a></li>
-          <li><a href="https://bitsofsimplicity.com/"><BlogIcon/><span>Blog</span></a></li>
+            </a>
+          </li>
+          <li>
+            <a href="/resume.pdf" aria-label="CV">
+              <CvIcon/>
+              <span>CV</span>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/bryan-haskin/" aria-label="LinkedIn">
+              <LinkedIn/>
+              <span>LinkedIn</span>
+            </a>
+          </li>
+          <li>
+            <a rel="me" href="https://social.bitsofsimplicity.com/@bhhaskin" aria-label="Mastodon">
+              <MastodonIcon/>
+              <span>Mastodon</span>
+            </a>
+          </li>
+          <li>
+            <a href="https://bitsofsimplicity.com/" aria-label="Blog">
+              <BlogIcon/>
+              <span>Blog</span>
+            </a>
+          </li>
         </ul>
       </div>
     </main>
